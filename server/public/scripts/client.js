5226d6d3ac7694905d25e1f7ed531f167941017c
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-07-20 16:24:28 -0500

    progress

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,86 @@
 $(document).ready(handleReady);
 
+let guessTotal = 0;
+let guessResults;
+
 function handleReady() {
   console.log("jquery is loaded!")
+
+  $('#submitBtn').on("click", handleSubmit);
+}
+
+
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // getting input from form
+
+  const player1Guess = $('#player1Guess').val();
+  const player2Guess = $('#player2Guess').val();
+  const player3Guess = $('#player3Guess').val();
+
+  let inputGuesses = {
+    player1: {
+      name: "Player 1",
+      guess: player1Guess
+    },
+    player2: {
+      name: "Player 2",
+      guess: player2Guess
+    },
+    player3: {
+      name: "Player 3",
+      guess: player3Guess
+    }
+
+  }
+  console.log(inputGuesses);
+
+  // AJAX post
+  $.ajax({
+    method: "POST",
+    url: '/game',
+    data: inputGuesses
+  }).then((response) => {
+    updateTable()
+    console.log("POST was successful:", response)
+  }).catch((error) => {
+    // console.log('error caught', error)
+    alert('ERRORRRRR')
+  })
+
+  guessTotal++
+  $('#guessTotal').text(guessTotal)
+
+}
+
+
+function updateTable() {
+// AJAX get
+$.ajax({
+  method: "GET",
+  url: '/input',
+}).then((response) => {
+  guessResults = response
+  console.log("GET was successful:", response)
+  render()
+}).catch((error) => {
+  // console.log('error caught', error)
+  alert('ERRORRRRR')
+})
+
+}
+
+function render() {
+for (let results in guessResults) {
+  $('#guessTable').append(`
+  <tr>
+  <td>${guessResults[results].name}</td>
+  <td>${guessResults[results].guess}</td>
+  <td>${guessResults[results].hint}</td>
+</tr>
+  `)
+}
+ 
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let theNUMBER;
+let inputGuesses;
+generateRandom();
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,6 +14,44 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+
+app.post('/game', (req,res) => {
+  console.log('at game req body', req.body)
+  inputGuesses = req.body
+  checkAgainstRandom(inputGuesses)
+  res.send(201)
+})
+
+
+app.get('/input', (req,res) => {
+res.send(inputGuesses)
+})
+
+
+function generateRandom() {
+  theNUMBER = Math.floor(Math.random()*(25-1)+1)
+  console.log('the secret number is: ',theNUMBER)
+}
+
+
+function checkAgainstRandom(allguesses) {
+  for ( let guess in allguesses) {
+  if (theNUMBER == allguesses[guess].guess) {
+    allguesses[guess].hint = 'Correct!!!' 
+  }
+  else if (theNUMBER < allguesses[guess].guess) {
+    allguesses[guess].hint = 'You are too high!'
+  }
+  else if (theNUMBER > allguesses[guess].guess) {
+    allguesses[guess].hint = 'You are too low!'
+  }
+  
+}
+console.log(allguesses);
+return allguesses;
+}
+
+console.log("input guesses after addind", inputGuesses);
 
 
 app.listen(PORT, () => {
